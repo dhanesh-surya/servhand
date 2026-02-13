cecfe0d15bd036d66503cb54805c1d90f64b0d95
unknown <digidhanesh@gmail.com>
2026-02-13 09:48:37 +0530

    Add servhand.onrender.com and *.onrender.com to ALLOWED_HOSTS default

--- shsite/shsite/settings.py
+++ shsite/shsite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-b(1=x%@&9h+eupnvi)_g$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,servhand.onrender.com,*.onrender.com', cast=Csv())
 
 
 # Application definition
